19ea9131272a0b0bd5f7db4d38cd74c4eae1d63a
Pengcheng Tang <tupachydralisk@gmail.com>
2016-01-13 14:53:43 +0800

    make schedule interval configurable through env var.

--- cron-cleaner/cron.py
+++ cron-cleaner/cron.py
@@ -14,18 +14,18 @@ def main():
   parser = optparse.OptionParser()
   parser.add_option('-t', '--time-unit', action="store", dest="time_unit", help="time unit", default="days")
   parser.add_option('-o', '--older-than', action="store", dest="older_than", help="retention length", default="7")
-  parser.add_option('-s', '--schedule-unit', action="store", dest="schedule_unit", help="schedule unit", default="day")
+  parser.add_option('-s', '--schedule-internval', action="store", dest="schedule_interval", help="schedule interval", default="every().day")
   options, args = parser.parse_args()
 
   # Use env vars as parameters to launch cron jobs
   time_unit = os.getenv('CURATOR_TIME_UNIT', "days")
-  newer_than = os.getenv('CURATOR_OLDER_THAN', "7")
-  schedule_unit = os.getenv('SCHEDULE_UNIT', "day")
+  older_than = os.getenv('CURATOR_OLDER_THAN', "7")
+  schedule_interval = os.getenv('SCHEDULE_INTERVAL', "every().day")
   host_addr = os.getenv("HOST_ADDR", "localhost")
 
   # for timestring format, we use something like this: "logstash-2016.01.13".
   cmd = "curator --host=%s delete indices --older-than %s --time-unit %s --timestring %%Y.%%m.%%d" % (host_addr, older_than, time_unit)
-  crontab = "schedule.every().%s.do(job, cmd)" % schedule_unit
+  crontab = "schedule.%s.do(job, cmd)" % schedule_interval
 
   eval(crontab)
   while 1:
