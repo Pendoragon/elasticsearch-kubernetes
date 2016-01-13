8d8e6ac80328f86fe33aa63ee0a70acbf23e2eca
Pengcheng Tang <tupachydralisk@gmail.com>
2016-01-13 11:21:25 +0800

    add optparse so that the cron job is somewhat more configurable.

--- cron-cleaner/cron.py
+++ cron-cleaner/cron.py
@@ -1,14 +1,30 @@
 import schedule
 import time
+import optparse
+
 from subprocess import call
 
-def job():
+def job(cmd):
   print("Cleaning up old data for elasticsearch...")
   call(cmd.split(" "))
 
-cmd = "curator --host=localhost delete indices --older-than 7 --time-unit days --timestring %Y/%m/%d"
-schedule.every().week.do(job)
+def main():
+  parser = optparse.OptionParser()
+  parser.add_option('-t', '--time-unit', action="store", dest="time_unit", help="time unit", default="days")
+  parser.add_option('-o', '--older-than', action="store", dest="older_than", help="retention length", default="7")
+  parser.add_option('-s', '--schedule-unit', action="store", dest="schedule_unit", help="schedule unit", default="day")
+  options, args = parser.parse_args()
 
-while 1:
-  schedule.run_pending()
-  time.sleep(1)
+  print 'time_unit:', options.time_unit
+  print 'older_than:', options.older_than
+
+  cmd = "curator --host=localhost delete indices --newer-than %s --time-unit %s --timestring %%Y/%%m/%%d" % (options.older_than, options.time_unit)
+  crontab = "schedule.every().%s.do(job, cmd)" % options.schedule_unit
+
+  eval(crontab)
+  while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
+if __name__ == "__main__":
+  main()
