640f602b8131ad1d4b7f5982394e64d0a5ffd2d4
Pengcheng Tang <tupachydralisk@gmail.com>
2016-01-13 11:57:40 +0800

    shrink image size & use env vars as parameters.

--- cron-cleaner/cron.py
+++ cron-cleaner/cron.py
@@ -1,25 +1,29 @@
 import schedule
 import time
+import datetime
 import optparse
-
+import os
 from subprocess import call
 
 def job(cmd):
-  print("Cleaning up old data for elasticsearch...")
+  print("[%s] Cleaning up old data for elasticsearch..." % datetime.datetime.now())
   call(cmd.split(" "))
 
 def main():
+  # The flags are mainly for testing purpose
   parser = optparse.OptionParser()
   parser.add_option('-t', '--time-unit', action="store", dest="time_unit", help="time unit", default="days")
-  parser.add_option('-o', '--older-than', action="store", dest="older_than", help="retention length", default="7")
+  parser.add_option('-o', '--newer-than', action="store", dest="newer_than", help="retention length", default="7")
   parser.add_option('-s', '--schedule-unit', action="store", dest="schedule_unit", help="schedule unit", default="day")
   options, args = parser.parse_args()
 
-  print 'time_unit:', options.time_unit
-  print 'older_than:', options.older_than
+  # Use env vars as parameters to launch cron jobs
+  time_unit = os.getenv('CURATOR_TIME_UNIT', "days")
+  newer_than = os.getenv('CURATOR_NEWER_THAN', "7")
+  schedule_unit = os.getenv('SCHEDULE_UNIT', "day")
 
-  cmd = "curator --host=localhost delete indices --newer-than %s --time-unit %s --timestring %%Y/%%m/%%d" % (options.older_than, options.time_unit)
-  crontab = "schedule.every().%s.do(job, cmd)" % options.schedule_unit
+  cmd = "curator --host=localhost delete indices --newer-than %s --time-unit %s --timestring %%Y/%%m/%%d" % (newer_than, time_unit)
+  crontab = "schedule.every().%s.do(job, cmd)" % schedule_unit
 
   eval(crontab)
   while 1:
